fb8f6638c51ba3a7031e058e1206fc275bdb099d
H3cJP <62990101+H3cJP@users.noreply.github.com>
2021-11-16 19:17:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from discord.ext import commands
 from AntiScam import AntiScam
 
-whitelist_s4vi = [500430447105540109,432228347074838538,871889282653106217,524301093014994956,270904126974590976,776030790189973524,667161053150445572,435067826483494934,400007985000087572,638410936457101315,430842153048997901,109060495348092928,685461041215307796]
+whitelist = [] # Here you can add the IDs of the users allowed to bypass the AntiScam system.
+logs_channel = None # Here you can add the ID of the channel where the logs will be sent.
 
-s4vibot = commands.Bot(command_prefix='>')
-s4vibot.remove_command('help')
+bot = commands.Bot(command_prefix='>')
+bot.remove_command('help') # Remove this line if you want to use the help command.
 
-@s4vibot.listen()
+@bot.listen()
 async def on_message(message):
-    await AntiScam(message, bot = s4vibot, whitelist = whitelist_s4vi, muted_role='Muted', verified_role='Verificado', logs_channel=884844932307763310)
+    await AntiScam(message, bot = bot, whitelist = whitelist, muted_role='Muted', verified_role='Verified', logs_channel=logs_channel) # Here you can change the names of the roles.
 
-s4vibot.run('ODcxODg5MjgyNjUzMTA2MjE3.YQh4Kw.t7WiaYgY-3_uBRkWVE3O1g85Yv4')
+bot.run('<bot-token>')
